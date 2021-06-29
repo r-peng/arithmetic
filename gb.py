46e9a6505b38d369555768fd368b62f3c0d6ddd3
Ruojing Peng <rppeng@pauling.caltech.edu>
2021-06-29 13:28:55 -0700

    before cleaning up

--- gb.py
+++ gb.py
@@ -13,32 +13,6 @@ def _angles(L,gamma=0.5):
         tmp = math.tan(2*math.pi*(j+1)/(2*L+1))*fac
         phi.append(2*math.atan(tmp)-math.pi)
     return phi
-def _S(a):
-    return np.array([[np.exp(1j*a),0],[0,1]],dtype=complex)
-def _R(x): # Rio=[exp(-iX*x)]io
-    R  = np.eye(2,dtype=complex)*math.cos(x)
-    R -= 1j*X*math.sin(x)
-    return R
-def _Rs(x): # Rxio=[exp(-iX*x)]io
-    R = np.zeros((len(x),2,2),dtype=complex)
-    for i in range(len(x)):
-        R[i,...] = _R(x[i])
-    return R
-def _wx(R,w,angles):
-    Rw = _R(w)
-    # gb
-    gb = np.einsum('xik,kj,kpq->xijpq',R,Rw,CiX)
-    # oaa
-    def _SA(angle,A,AL):
-        S = _S(angle)
-        AL = np.einsum('xipq,ij->xjpq',AL,S)
-        return np.einsum('xipr,xijrq->xjpq',AL,A)
-    L = len(angles)
-    AL = np.einsum('xijpq,i->xjpq',gb,ZERO)
-    for j in range(L):
-        AL = _SA(angles[L-1-j],gb.transpose(0,2,1,4,3).conj(),AL)
-        AL = _SA(angles[j], gb, AL)
-    return np.einsum('xipq,i->xpq',AL,ZERO)
 def _svd(T,naxis,thresh=1e-10): # svd after the 1st naxis
     tdim = T.shape
     mdim1 = reduce(operator.mul,tdim[:naxis]) 
@@ -47,7 +21,69 @@ def _svd(T,naxis,thresh=1e-10): # svd after the 1st naxis
     s = s[s>thresh]
     u = np.reshape(u[:,:len(s)],tdim[:naxis]+(len(s),))
     vh = np.reshape(vh[:len(s),:],(len(s),)+tdim[naxis:])
-    return u, s, vh
+    return np.einsum('...a,a->...a',u,s),vh
+def _S(a):
+    return np.array([[np.exp(1j*a),0],[0,1]],dtype=complex)
+def _R(x): # Rio=[exp(-iX*x)]io
+    return np.eye(2,dtype=complex)*math.cos(x)-1j*X*math.sin(x)
+def _Rs(x,thresh=1e-10): 
+    R = np.zeros((len(x),2,2),dtype=complex)
+    for i in range(len(x)):
+        R[i,...] = _R(x[i])
+    x,v = _svd(R,1,thresh)
+    return [x,v]
+def _add_input(Rk,R,thresh=1e-10):
+    v = Rk.pop()
+    A = np.einsum('aij,xb,bjk->axik',v,R[0],R[1])
+    x,v = _svd(A,2,thresh)
+    return Rk + [x,v]
+def _compound(y1,y2,thresh=1e-10):
+    assert len(y1) == len(y2)
+    n = len(y1) - 1
+    ls = []
+    xi = np.einsum('xa,xb->xab',y1[0],y2[0])
+    for i in range(1,n):
+        A = np.einsum('...xab,ayc,byd->...xycd',xi,y1[i],y2[i])
+        xim,xi = _svd(A,len(A.shape)-3,thresh)
+        ls.append(xim)
+    return ls,xi
+def _add_hidden(y1,y2,thresh=1e-10):
+    ls,xn = _compound(y1,y2,thresh)
+    A = np.einsum('...ab,aij,bjk->...ik',xn,y1[-1],y2[-1])
+    xn,v = _svd(A,len(A.shape)-2,thresh)
+    return ls + [xn,v]
+def _oaa(gb,angles,thresh=1e-10):
+    gbt = [x.conj() for x in gb]
+    gbt[-1] = gbt[-1].transpose(0,2,1,4,3)
+    L = len(angles)
+
+    xkG,xnG = _compound(gbt,gb,thresh)
+    def _G(j):
+        svt = np.einsum('ij,ajkpq->aikpq',_S(angles[L-1-j]),gbt[-1])
+        sv = np.einsum('ij,ajkpq->aikpq',_S(angles[j]),gb[-1])
+        A = np.einsum('...ab,aijpr,bjkrq->...ikpq',xnG,svt,sv)
+        xn,v = _svd(A,len(A.shape)-4,thresh)
+        return [xn,v]
+    AL = gb.copy() 
+    AL[-1] = np.einsum('aijpq,i->ajpq',AL[-1],ZERO)
+    for j in range(L):
+        G = xkG + _G(j)
+        xkL,xnL = _compound(AL,G,thresh)
+        A = np.einsum('...ab,akpr,bkjrq->...jpq',xnL,AL[-1],G[-1])
+        xnL,vL = _svd(A,len(A.shape)-3,thresh)
+        AL = xkL + [xnL,vL]
+    AL[-1] = np.einsum('ajpq,j->apq',AL[-1],ZERO)
+    return AL
+def _scalar_mult(R,w,angles,thresh=1e-10):
+    Rw = _R(w)
+    gb = R.copy()
+    gb[-1] = np.einsum('aij,jpq,jk->aikpq',gb[-1],CiX,Rw)
+    return _oaa(gb,angles,thresh)
+def _node_mult(y1,y2,angles,thresh=1e-10):
+    xk,xn = _compound(y1,y2,thresh)
+    A = np.einsum('...ab,aij,bjk,jpq->...ikpq',xn,y1[-1],y2[-1],CiX)
+    xn,v = _svd(A,len(A.shape)-4,thresh)
+    return _oaa(xk+[xn,v],angles,thresh)
 def _sq(Rs,angles,thresh=1e-10):
     # gb
     n = len(Rs)
@@ -87,6 +123,4 @@ def _sq(Rs,angles,thresh=1e-10):
         RLs = _SA(angles[j],RLs,hc=False)
     RLs[0] = np.einsum('ixa,i->xa',RLs[0],ZERO)
     return RLs
-def _mult_y(R1s,R2s,angles,thresh_1e-10):
-    n = len(R1s)
      
--- test_gb.py
+++ test_gb.py
@@ -1,9 +1,10 @@
 import numpy as np
 import math, statistics
 import gb
-d = 3
+d = 10
 epsilon = 0.01
 L = 10 
+thresh = 1e-12
 
 ZERO = np.array([1,0],dtype=complex)
 X = np.array([[0,1],[1,0]],dtype=complex)
@@ -12,30 +13,156 @@ CiX[0,...] = np.eye(2,dtype=complex)
 CiX[1,...] = 1j*X
 angles = gb._angles(L)
 
-#x = [np.random.rand()*epsilon for i in range(d)]
-#R = gb._Rs(x)
-#w = np.random.rand()*epsilon
-#R = gb._wx(R,w,angles)
-#err = 0
-#angs = []
-#phases = []
-#for i in range(len(x)):
-#    tan = 1j*R[i,1,0]/R[i,0,0]
+#print('########### check decomp ##############')
+#x = [np.random.rand() for i in range(d)]
+#R = gb._Rs(x,thresh)
+#R_ = np.einsum('xa,aij->xij',R[0],R[1])
+#err = 0.0
+#for i in range(d):
+#    R = R_[i,...]
+#    tan = 1j*R[1,0]/R[0,0]
 #    angle = math.atan(tan.real)
-#    err += tan.imag
-#    print(i, w*x[i], angle, abs((w*x[i]-angle)/(w*x[i])))
-#    angs.append(abs((w*x[i]-angle)/(w*x[i])))
+#    err += abs(x[i]-angle)
 #    U = gb._R(angle)
-#    tmp = np.divide(R[i,...],U).flatten()
+#    tmp = np.divide(R,U).flatten()
+#    err += abs(1.0-tmp[0])
 #    err += statistics.pstdev(list(tmp.real))
 #    err += statistics.pstdev(list(tmp.imag))
-#    phases.append(tmp[0])
-#phases = np.array(phases,dtype=complex)
 #print(err)
-#print(statistics.mean(list(phases.real)),statistics.pstdev(list(phases.real)))
-#print(statistics.mean(list(phases.imag)),statistics.pstdev(list(phases.imag)))
-#print(statistics.mean(angs))
 #exit()
+#print('########### check addition ##############')
+#x1 = [np.random.rand()*epsilon for i in range(d)]
+#x2 = [np.random.rand()*epsilon for i in range(d)]
+#R1 = gb._Rs(x1,thresh)
+#R2 = gb._Rs(x2,thresh)
+#R12 = gb._add_input(R1,R2,thresh)
+#print(R12[0].shape, R12[1].shape, R12[2].shape)
+#R12_ = np.einsum('xa,ayb,bij->xyij',R12[0],R12[1],R12[2])
+#err = 0.0
+#for i in range(d):
+#    for j in range(d):
+#        R = R12_[i,j,...]
+#        tan = 1j*R[1,0]/R[0,0]
+#        angle = math.atan(tan.real)
+#        err += abs(tan.imag)
+#        err += abs(x1[i]+x2[j]-angle)
+#        U = gb._R(angle)
+#        tmp = np.divide(R,U).flatten()
+#        err += abs(1.0-tmp[0])
+#        err += statistics.pstdev(list(tmp.real))
+#        err += statistics.pstdev(list(tmp.imag))
+#print(err)
+#R12 = gb._add_hidden(R12,R12,thresh)
+#print(R12[0].shape, R12[1].shape, R12[2].shape)
+#R12_ = np.einsum('xa,ayb,bij->xyij',R12[0],R12[1],R12[2])
+#err = 0.0
+#for i in range(d):
+#    for j in range(d):
+#        R = R12_[i,j,...]
+#        tan = 1j*R[1,0]/R[0,0]
+#        angle = math.atan(tan.real)
+#        err += abs(tan.imag)
+#        err += abs((x1[i]+x2[j])*2-angle)
+#        U = gb._R(angle)
+#        tmp = np.divide(R,U).flatten()
+#        err += abs(1.0-tmp[0])
+#        err += statistics.pstdev(list(tmp.real))
+#        err += statistics.pstdev(list(tmp.imag))
+#print(err)
+#R12 = gb._add_hidden(R12,R12,thresh)
+#print(R12[0].shape, R12[1].shape, R12[2].shape)
+#R12_ = np.einsum('xa,ayb,bij->xyij',R12[0],R12[1],R12[2])
+#err = 0.0
+#for i in range(d):
+#    for j in range(d):
+#        R = R12_[i,j,...]
+#        tan = 1j*R[1,0]/R[0,0]
+#        angle = math.atan(tan.real)
+#        err += abs(tan.imag)
+#        err += abs((x1[i]+x2[j])*4-angle)
+#        U = gb._R(angle)
+#        tmp = np.divide(R,U).flatten()
+#        err += abs(1.0-tmp[0])
+#        err += statistics.pstdev(list(tmp.real))
+#        err += statistics.pstdev(list(tmp.imag))
+#print(err)
+#exit()
+print('################ check multiplication ################')
+def _num_oaa(x):
+    A = np.array([[x,math.sqrt(1-x**2)],
+               [math.sqrt(1-x**2),-x]])
+    AL = np.array(A,dtype=complex)
+    for j in range(L):
+        Sl = np.array([np.exp(1j*angles[j]),1],dtype=complex)
+        Sl = np.diag(Sl)
+        j_ = L-1-j
+        Sr = np.array([np.exp(1j*angles[j_]),1],dtype=complex)
+        Sr = np.diag(Sr)
+        G = np.linalg.multi_dot([A,Sl,A,Sr])
+        AL = np.dot(G,AL)
+    return AL[0,0]
+x1 = [np.random.rand()*epsilon for i in range(d)]
+x2 = [np.random.rand()*epsilon for i in range(d)]
+R1 = gb._Rs(x1,thresh)
+R2 = gb._Rs(x2,thresh)
+R12 = gb._add_input(R1,R2,thresh)
+w = np.random.rand()*epsilon
+R12w = gb._scalar_mult(R12,w,[],thresh)
+R12wL = gb._scalar_mult(R12,w,angles,thresh)
+R12sq = gb._node_mult(R12,R12,[],thresh)
+R12sqL = gb._node_mult(R12,R12,angles,thresh)
+print(R12wL[0].shape,R12wL[1].shape,R12wL[2].shape)
+print(R12sqL[0].shape,R12sqL[1].shape,R12sqL[2].shape)
+def gb_err(y,yL,f):
+    err = 0
+    angs = []
+    phases = []
+    for i in range(d):
+        i1 = np.zeros(d,dtype=complex)
+        i1[i] = 1.0
+        R1 = np.einsum('xa,x->a',y[0],i1)
+        R1L = np.einsum('xa,x->a',yL[0],i1)
+        for j in range(d):
+            i2 = np.zeros(d,dtype=complex)
+            i2[j] = 1.0
+            R2 = np.einsum('axb,x->ab',y[1],i2)
+            R2L = np.einsum('axb,x->ab',yL[1],i2)
+    
+            R = np.einsum('a,ab,bpq->pq',R1,R2,y[-1])
+            RL = np.einsum('a,ab,bpq->pq',R1L,R2L,yL[-1])
+            tan = 1j*R[1,0]/R[0,0]
+            tanL = 1j*RL[1,0]/RL[0,0]
+            angle = math.atan(tan.real)
+            angleL = math.atan(tanL.real)
+            err += abs(angle-angleL)
+            err += abs(tan.imag)
+            err += abs(tanL.imag)
+            exact = f(x1[i],x2[j]) 
+            angs.append(abs((exact-angle)/exact))
+            U = gb._R(angle)
+            tmp = np.divide(R,U).flatten()
+            tmpL = np.divide(RL,U).flatten()
+            err += tmp[0].imag
+    #        err += statistics.pstdev(list(tmp.real))
+    #        err += statistics.pstdev(list(tmp.imag))
+    #        err += statistics.pstdev(list(tmpL.real))
+    #        err += statistics.pstdev(list(tmpL.imag))
+            amp = _num_oaa(tmp[0].real)
+            err += abs(tmpL[0]-amp)
+            phases.append(tmpL[0])
+    phases = np.array(phases,dtype=complex)
+    print(err)
+    print(statistics.mean(list(phases.real)),statistics.pstdev(list(phases.real)))
+    print(statistics.mean(list(phases.imag)),statistics.pstdev(list(phases.imag)))
+    print(statistics.mean(angs))
+def f1(x1,x2):
+    return w*(x1+x2)
+def f2(x1,x2):
+    return (x1+x2)**2
+gb_err(R12w,R12wL,f1)
+gb_err(R12sq,R12sqL,f2)
+exit()
+print('########### check SVD #########################')
 ##print(R1.shape, R2.shape)
 x1 = [np.random.rand()*epsilon for i in range(d)]
 x2 = [np.random.rand()*epsilon for i in range(d)]
@@ -44,7 +171,6 @@ R1 = gb._Rs(x1)
 R2 = gb._Rs(x2)
 R3 = gb._Rs(x3)
 thresh = 1e-6
-print('########### check SVD #########################')
 ls1 = gb._sq([R1,R2,R3],[])
 ls2 = gb._sq([R1,R2,R3],angles)
 err1 = 0
