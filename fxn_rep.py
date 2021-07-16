5804fec930d384f3dcd38b1a490dca81536f1fc0
Ruojing Peng <rppeng@pauling.caltech.edu>
2021-07-16 11:33:56 -0700

    before removing + counts

--- fxn_rep.py
+++ fxn_rep.py
@@ -1,7 +1,7 @@
 import numpy as np
 import quimb.tensor as qtn
+import quimb.tensor.tensor_core as qtc
 import cotengra as ctg
-
 # basic tensors
 def _delta(d):
     out = np.zeros((d,)*3)
@@ -17,56 +17,77 @@ def _const_fxn(ds,a,label=''):
     xs = [_tensor(np.ones(di)) for di in ds]
     xs[0][:,1] *= a
     tn = qtn.TensorNetwork([])
-    o = ''.join(['{},'.format(i) for i in range(n)])
+    o = qtc.rand_uuid()
     for i in range(len(xs)):
         i_ = '{},'.format(i)
-        inds = 'x'+i_, label+o
-        tn.add(qtn.Tensor(data=_tensor(xs[i]),inds=inds,tags={label,inds[0]}))
-    return qtn.TensorNetwork(ls) 
+        inds = 'x'+i_,o
+        xi = qtn.Tensor(data=_tensor(xs[i]),inds=inds,tags={label,'x',i_})
+        tn.add_tensor(xi)
+        tn._outer_inds.add(inds[0])
+    tn._outer_inds.add(o)
+    return tn
 ADD = np.zeros((2,2,2)) # i1,i2,o
 ADD[0,0,0] = ADD[0,1,1] = ADD[1,0,1] = 1.0
 #MUL = _delta(2) # use hyperedge
 #DEL = _delta(d)
 # x1+...+xn
 def _train(xs,label=''):
-    ls = []
-    for i in range(len(xs)):
-        i_ = '{},'.format(i)
-        inds = 'x'+i_, label+i_
-        ls.append(qtn.Tensor(data=_tensor(xs[i]),inds=inds,tags={label,inds[0]}))
-    inds = ['0,']
+    tn = qtn.TensorNetwork([])
+    indx = 'x0,',qtc.rand_uuid()
+    xi = qtn.Tensor(data=_tensor(xs[0]),inds=indx,tags={label,'x','0,'})
+    indp = (indx[-1],)
+    tn.add_tensor(xi,tid=0,virtual=False)
+    tn._outer_inds.add(indx[0])
+    tn._inner_inds.add(indx[-1])
     for i in range(1,len(xs)):
         i_ = '{},'.format(i)
-        inds = inds[-1],i_,inds[-1]+i_
-        ls.append(qtn.Tensor(data=ADD,inds=[label+i for i in inds],tags={label,'+'}))
-    return ls
-def _chebyshev(n,tn,typ,p,label=''):
-    # n: number of variables
+        indx = 'x'+i_,qtc.rand_uuid()
+        xi = qtn.Tensor(data=_tensor(xs[i]),inds=indx,tags={label,'x',i_})
+        indp = indp[-1],indx[-1],qtc.rand_uuid()
+        ai = qtn.Tensor(data=ADD,inds=indp,tags={label,'+',i_})
+        tn.add_tensor(xi)
+        tn.add_tensor(ai)
+        tn._outer_inds.add(indx[0])
+        tn._inner_inds.add(indx[-1])
+        if i==len(xs)-1:
+            tn._outer_inds.add(indp[-1])
+        else:
+            tn._inner_inds.add(indp[-1])
+    return tn
+def _mult_const(tn,a): #inplace
+    n = len(tn._outer_inds)-1
+    keys = set(['{},'.format(i) for i in range(n)]+['x','+'])
+    labels = set(tn.tag_map.keys())-keys
+
+    data = np.array([1.0,a])
+    inds = [tn._outer_inds[-1]]
+    tags = labels+{'const'}
+    tn.add_tensor(qtn.Tensor(data=data,inds=inds,tags=tags))
+    return tn
+def _chebyshev(tn,typ,p,label=''):
     # tn: quimb list
     # typ: 't' or 'u'
     # p: highest degree
-    ds = [tn[i].data.shape[0] for i in range(n)]
+    n = len(tn._outer_inds)-1
+    tids = tn._get_tids_from_tags(tags='x',which='any')
+    ds = [tn[i].data.shape[0] for i in tids]
     p0 = _const(ds,1,label)
-    p1 = tn.copy()
-    if typ=='u'or'U':
-        data = np.array([1.0,2.0])
-        inds = [p1[-1].inds[-1]]
-        tags = {label,'2'}
-        p1.append(qtn.Tensor(data=data,inds=inds,tags=tags))
-    
+    p1 = tn.copy(deep=True)
+#    if typ=='u'or'U':
+#        ls = [p0,p1]
+#    for i in range(2,p+1):
+#        tn1,tn2 = ls[-1],ls[-2] 
 if __name__=='__main__':
     d = 3
     n = 3
     xs = [np.random.rand(d) for i in range(n)]
     tn = _train(xs)
-    output_inds = [tn[i].inds[0] for i in range(n)]+[tn[-1].inds[-1]]
-    print(output_inds)
     opt = ctg.HyperOptimizer()
-    tn = qtn.TensorNetwork(tn)
-    print(tn)
-    out = tn.contract(output_inds=output_inds,optimize=opt)
+    out = tn.contract(output_inds=tn._outer_inds,optimize=opt)
     ins = [np.random.randint(0,d) for i in range(n)]
     true = sum([xs[i][ins[i]] for i in range(n)])
     print('check train: ', true-out.data[...,1][tuple(ins)])
     print('check train: ', 1.0-out.data[...,0][tuple(ins)])
-     
+    print(tn.tag_map)
+    print(tn._outer_inds)
+    print(tn._get_tids_from_tags(tags='x',which='any'))
