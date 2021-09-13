ba416afa366848694d28587ab61cc065a79c717f
Ruojing Peng <rppeng@pauling.caltech.edu>
2021-09-13 11:16:00 -0700

    quartic optimized, before changing nn_mop

--- arithmetic/nn_mpo.py
+++ arithmetic/nn_mpo.py
@@ -1,5 +1,7 @@
 import numpy as np
 import quimb.tensor as qtn
+CP2 = np.zeros((2,)*3)
+CP2[0,0,0] = CP2[1,1,1] = 1.0
 ADD = np.zeros((2,)*3)
 ADD[0,0,0] = ADD[0,1,1] = ADD[1,0,1] = 1.0
 def get_CP(d):
@@ -7,14 +9,78 @@ def get_CP(d):
     for i in range(d):
         out[i,i,i] = 1.0
     return out
-def get_weighted_sum1(Xs,ws,b):
+def get_weighted_sum1(X,ws,b):
     # weighted sum
     arrays = []
-    for i in range(len(Xs)):
-        tmp = np.einsum('ipq,i->ipq',Xs[i],np.array([1.0,ws[i]))
+    for i in range(len(ws)):
+        tmp = np.einsum('ipq,i->ipq',X,np.array([1.0,ws[i]]))
         if i>0:
             tmp = np.einsum('ipq,ijk->jkpq',tmp,ADD)
         arrays.append(tmp)
-    tmp = np.einsum('ijk,j->ik',ADD,np.array([1.0,b]))
+    tmp = np.einsum('ijk,i->jk',ADD,np.array([1.0,b]))
     arrays.append(tmp)
     return arrays
+def get_weighted_sum(hs,ws,b,**compress_opts):
+    arrays = [t.copy() for t in hs[0]]
+    tmp = np.einsum('ijk,j->ik',CP2,np.array([1.0,ws[0]])).reshape(2,1,2)
+    arrays[-1] = np.einsum('...i,ijk->...jk',arrays[-1],tmp)
+    arrays = [np.ascontiguousarray(array) for array in arrays]
+    mpo = qtn.MatrixProductOperator(arrays,shape='lrud')
+    for i in range(1,len(ws)):
+        arrays = [t.copy() for t in hs[i]]
+        tmp = np.einsum('ijk,klm,j->ilm',CP2,ADD,np.array([1.0,ws[0]]))
+        arrays[-1] = np.einsum('...i,ijk->...jk',arrays[-1],tmp)
+        arrays = [np.ascontiguousarray(array) for array in arrays]
+        other = qtn.MatrixProductOperator(arrays,shape='lrud')
+        mpo = mpo._apply_mpo(other,compress=True,**compress_opts)
+    arrays = list(mpo.tensor_map.values())
+    arrays = [array.data for array in arrays]
+    tmp = np.einsum('ijk,j->ik',ADD,np.array([1.0,b]))
+    arrays[-1] = np.einsum('li,ik->lk',arrays[-1][:,0,:],tmp)
+    return arrays
+def get_pol(h,coeff,**compress_opts):
+    for i in range(len(coeff)-2,-1,-1):
+        arrays = [t.copy() for t in h]
+        tmp = np.einsum('ijk,klm,l->ijm',CP2,ADD,np.array([1.0,coeff[i]])) 
+        arrays[-1] = np.einsum('...i,ijk->...jk',arrays[-1],tmp)
+        arrays = [np.ascontiguousarray(array) for array in arrays]
+        uind,lind = 'u{},'.format(i),'l{},'.format(i)
+        if i==len(coeff)-2:
+            mpo = qtn.MatrixProductOperator(arrays,shape='lrud',
+                  upper_ind_id=uind+'{}',lower_ind_id=lind+'{}')
+            print(mpo)
+        else:
+            other = qtn.MatrixProductOperator(arrays,shape='lrud',
+                  upper_ind_id=uind+'{}',lower_ind_id=lind+'{}')
+            print(other)
+            mpo = mpo._apply_mpo(other,compress=,**compress_opts)
+            print(mpo)
+    arrays = list(mpo.tensor_map.values())
+    arrays = [array.data for array in arrays]
+    arrays[-1] = np.einsum('lud,u->ld',arrays[-1],np.array([1.0,coeff[-1]]))
+    return arrays
+def get_layer1(xs,W,B,coeff,**compress_opts):
+    # hi = Wijxj+Bi
+    d = len(xs)
+    X = np.zeros((d,2))
+    X[:,0] = np.ones(d)
+    X[:,1] = xs.copy()
+    X = np.einsum('ri,pqr->ipq',X,get_CP(d))
+    ls = []
+    for i in range(len(B)):
+        h = get_weighted_sum1(X,W[i,:],B[i])
+        ls.append(get_pol(h,coeff,**compress_opts))
+    return ls
+def get_layer(hs,W,B,coeff,**compress_opts):
+    ls = []
+    for i in range(len(B)):
+        h = get_weighted_sum(hs,W[i,:],B[i],**compress_opts)
+        ls.append(get_pol(h,coeff,**compress_opts))
+    return ls
+def integrate(h,tr):
+    out = np.einsum('rud,u,d->r',h[0],tr,np.ones_like(tr))
+    for i in range(1,len(h)-1):
+        tmp = np.einsum('lrud,u,d->lr',h[i],tr,np.ones_like(tr))
+        out = np.einsum('l,lr->r',out,tmp)
+    return np.dot(out,h[-1][:,1])
+    
--- arithmetic/quartic.py
+++ arithmetic/quartic.py
@@ -1,39 +1,56 @@
 import numpy as np
 import quimb.tensor as qtn
-
-
-def get_row(N,xmap,cexp,cpol=None):
+# done 1. get_off_diagonal/cap_left: dummy before first x
+# done 2. get_diagonal_row
+# 3. only 4-body term
+CP2 = np.zeros((2,)*3)
+CP2[0,0,0] = CP2[1,1,1] = 1.0 
+ADD = np.zeros((2,)*3)
+ADD[0,0,0] = ADD[0,1,1]= ADD[1,0,1] = 1.0
+def get_diag(Qs):
+    row = []
+    for i in range(len(Qs)):
+        tmp = Qs[i].copy()
+        if i==0:
+            tmp = tmp[0,...].reshape((1,)+tmp.shape[1:])
+        row.append(tmp)
+    row.append(np.eye(2).reshape(2,1,1,2))
+    row.append(np.eye(2).reshape(1,2,2))
+    return row
+def get_off_diag(N,xmap,cexp,cpol=None):
     inds = list(xmap.keys())
     inds.sort()
     d = xmap[inds[0]].shape[-1]
     row = []
-    for i in range(len(inds)):
-        start = 0 if i==0 else inds[i-1]+1
-        stop = inds[i]
+    # dummies before the first x
+    for j in range(inds[0]):
+        row.append(np.eye(d).reshape(1,1,d,d))
+    tmp = np.einsum('i...,i->...',xmap[inds[0]],np.ones(2))
+    row.append(tmp.reshape((1,)+tmp.shape))
+    # dummies between 2 consecutive x's
+    for i in range(1,len(inds)):
+        start,stop = inds[i-1]+1,inds[i]
         for j in range(start,stop):
             row.append(np.einsum('ij,pq->ijpq',np.eye(2),np.eye(d)))
         row.append(xmap[stop])
     for j in range(inds[-1]+1,N):
         row.append(np.einsum('ij,pq->ijpq',np.eye(2),np.eye(d)))
-    CP = np.zeros((2,)*3)
-    CP[0,0,0] = CP[1,1,1] = 1.0 
-    ADD = np.zeros((2,)*3)
-    ADD[0,0,0] = ADD[0,1,1]= ADD[1,0,1] = 1.0
     coeff = np.array([1.0,cexp])
     if cpol is None:
-        row.append(np.einsum('ijk,klm,j->ilm',CP,ADD,coeff).reshape(2,1,2,2))
+        row.append(np.einsum('ijk,klm,j->ilm',CP2,ADD,coeff).reshape(2,1,2,2))
         row.append(np.eye(2).reshape(1,2,2))
     else:
-        row.append(np.einsum('ijk,klm,j->iml',CP,ADD,coeff).reshape(2,2,2,1))
+        row.append(np.einsum('ijk,klm,j->iml',CP2,ADD,coeff).reshape(2,2,2,1))
         coeff = np.array([1.0,cpol])
-        row.append(np.einsum('ijk,klm,l->ijm',CP,ADD,coeff))
+        row.append(np.einsum('ijk,klm,l->ijm',CP2,ADD,coeff))
     return row
 def cap_top(arrays,tr,an):
     row = arrays[0]
     N = len(row)-2
     for i in range(N):
         row[i] = np.einsum('...pq,p->...q',row[i],tr)
-    row[N] = np.einsum('...pq,p->...q',row[N],np.array([1.0]))
+#    row[N] = np.einsum('...pq,p->...q',row[N],np.array([1.0]))
+    row[N] = row[N][...,0,:]
     row[N+1] = np.einsum('...pq,p->...q',row[N+1],np.array([1.0,an]))
     arrays[0] = row
     return arrays
@@ -43,14 +60,17 @@ def cap_bottom(arrays):
     d = row[0].shape[-1]
     for i in range(N):
         row[i] = np.einsum('...q,q->...',row[i],np.ones(d))
-    row[N] = np.einsum('...q,q->...',row[N],np.array([1.0]))
-    row[N+1] = np.einsum('...q,q->...',row[N+1],np.array([0.0,1.0]))
+#    row[N] = np.einsum('...q,q->...',row[N],np.array([1.0]))
+    row[N] = row[N][...,0]
+#    row[N+1] = np.einsum('...q,q->...',row[N+1],np.array([0.0,1.0]))
+    row[N+1] = row[N+1][...,1]
     arrays[-1] = row
     return arrays
 def cap_left(arrays):
     nrows = len(arrays)
     for i in range(nrows):
-        arrays[i][0] = np.einsum('i...,i->...',arrays[i][0],np.ones(2))
+        d = arrays[i][0].shape[0]
+        arrays[i][0] = np.einsum('i...,i->...',arrays[i][0],np.ones(d))
     return arrays
 def to_peps(arrays):
     nrows,ncols = len(arrays),len(arrays[0])
@@ -74,32 +94,39 @@ def get_xmap(inds,Xs):
             count = 1
     xmap.update({old:Xs[count-1]})
     return xmap
-def get_block(N,Xs,ai,A,B=None):
-    arrays = []
+def get_block(N,Qs,Xs,ai,A,B=None):
+    arrays = [get_diag(Qs)]
     for i in range(N):
-        for j in range(N):
+        for j in range(i):
             xmap = get_xmap([i,j],Xs)
-            cpol = ai if i==j==N-1 and B is None else None 
-            arrays.append(get_row(N,xmap,A[i,j],cpol=cpol))
+            cpol = ai if i==N-1 and j==i-1 and B is None else None 
+            arrays.append(get_off_diag(N,xmap,A[i,j],cpol=cpol))
     if B is not None:
         for i in range(N):
-            for j in range(N):
-                for k in range(N):
-                    for l in range(N):
+            for j in range(i):
+                for k in range(j):
+                    for l in range(k):
                         xmap = get_xmap([i,j,k,l],Xs)
-                        cpol = ai if i==j==k==l==N-1 else None 
-                        arrays.append(get_row(N,xmap,B[i,j,k,l],cpol=cpol))
-    row = arrays[0]
-    row[-2] = row[-2][:,:,:1,:]
+                        cpol = ai if i==N-1 and j==i-1 and k==j-1 and l==k-1 else None 
+                        arrays.append(get_off_diag(N,xmap,B[i,j,k,l],cpol=cpol))
+#    row = arrays[0]
+#    row[-2] = row[-2][:,:,:1,:]
     return arrays
-def get_Xs(xs):
+def get_diag_tensors(xs,A):
     d = len(xs)
     CPd = np.zeros((d,)*3)
     for i in range(d):
         CPd[i,i,i] = 1.0
-    CP2 = np.zeros((2,)*3)
-    CP2[0,0,0] = CP2[1,1,1] = 1.0 
-
+    X = np.zeros((d,2))
+    X[:,1] = np.square(xs)
+    X[:,0] = np.ones(d)
+    return [np.einsum('pqr,ri,i,ijk->jkpq',CPd,X,np.array([1.0,a]),ADD) for a in A]
+def get_off_diag_tensors(xs):
+    # common tensors for off diagonal row
+    d = len(xs)
+    CPd = np.zeros((d,)*3)
+    for i in range(d):
+        CPd[i,i,i] = 1.0
     X1 = np.zeros((d,2))
     X1[:,1] = xs.copy()
     X1[:,0] = np.ones(d)
@@ -108,17 +135,19 @@ def get_Xs(xs):
     X4 = np.multiply(X3,X1)
     return [np.einsum('pqr,ri,ijk->jkpq',CPd,X,CP2) for X in [X1,X2,X3,X4]]
 def energy(N,xs,tr,A,B=None):
-    Xs = get_Xs(xs)
-    arrays = get_block(N,Xs,0.0,A,B)
+    Qs = get_diag_tensors(xs,list(0.5*np.diag(A)))
+    Xs = get_off_diag_tensors(xs)
+    arrays = get_block(N,Qs,Xs,0.0,A,B)
     arrays = cap_top(arrays,tr,1.0)
     arrays = cap_bottom(arrays)
     arrays = cap_left(arrays)
     return to_peps(arrays)
 def pol(N,xs,tr,coeff,A,B=None):
-    Xs = get_Xs(xs)
+    Qs = get_diag_tensors(xs,list(0.5*np.diag(A)))
+    Xs = get_off_diag_tensors(xs)
     arrays = []
     for i in range(len(coeff)-2,-1,-1):
-        arrays += get_block(N,Xs,coeff[i],A,B)
+        arrays += get_block(N,Qs,Xs,coeff[i],A,B)
     arrays = cap_top(arrays,tr,coeff[-1])
     arrays = cap_bottom(arrays)
     arrays = cap_left(arrays)
